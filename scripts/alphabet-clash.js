e058ba580ce34e0a86b4753623b035617aa779cb
Occult <maheermusfiq221@gmail.com>
2025-03-29 03:15:22 +0600

    added a functional play now button in the home screen

---
+++ scripts/alphabet-clash.js
@@ -0,0 +1,14 @@
+// function playNow(){
+//     // step-1: Hide the home screen.To hide the home screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+
+//     // step-2: Unhide the playground screen. To unhide the playground screen remove the class hidden from the playground section
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+// }
+
+function playNow(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+}
