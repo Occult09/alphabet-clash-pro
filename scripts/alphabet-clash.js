6d6836cb3f163e8724535a66c4cbac689ae8450b
Occult <maheermusfiq221@gmail.com>
2025-03-29 21:13:32 +0600

    score and life update half completed

--- scripts/alphabet-clash.js
+++ scripts/alphabet-clash.js
@@ -1,12 +1,52 @@
-// function playNow(){
-//     // step-1: Hide the home screen.To hide the home screen add the class hidden to the home section
-//     const homeSection = document.getElementById('home-screen');
-//     homeSection.classList.add('hidden');
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key;
 
-//     // step-2: Unhide the playground screen. To unhide the playground screen remove the class hidden from the playground section
-//     const playgroundSection = document.getElementById('play-ground');
-//     playgroundSection.classList.remove('hidden');
-// }
+    // get the expected button to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    // check if the alphabet matched or not
+    if (playerPressed === expectedAlphabet) {
+        console.log("You get a point");
+        // update score:
+
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        // 2. increase the score by 1
+        const newScore = currentScore + 1;
+
+        // 3. show the updated score
+        currentScoreElement.innerText = newScore;
+
+
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        console.log("You missed. You lost a life");
+        // update life
+
+        // 1. get the current life
+        const currentLifeElement = document.getElementById('life-score');
+        const currentLifeScoreText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeScoreText);
+
+        // 2. decrease the current life by 1
+        const newLife = currentLife - 1;
+
+        // 3. update the new life score
+        currentLifeElement.innerText = newLife;
+    }
+}
+
+// capture keyboard key press
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
 
 function continueGame() {
     // step-1: generate a random alphabet
